9ea93639b70b0136d7afc2c5dae303c934bbfcac
Tai D. Nguyen <duytai.cse@gmail.com>
2021-02-18 13:05:14 +0800

    record uncovered

--- llvm_mode/afl-llvm-pass.so.cc
+++ llvm_mode/afl-llvm-pass.so.cc
@@ -37,9 +37,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
+#include <set>
 
 #include "llvm/ADT/Statistic.h"
 #include "llvm/IR/IRBuilder.h"
@@ -77,7 +75,6 @@ bool AFLCoverage::runOnModule(Module &M) {
 
   LLVMContext &C = M.getContext();
 
-  Type *VoidTy = Type::getVoidTy(C);
   IntegerType *Int8Ty  = IntegerType::getInt8Ty(C);
   IntegerType *Int32Ty = IntegerType::getInt32Ty(C);
 
@@ -108,19 +105,16 @@ bool AFLCoverage::runOnModule(Module &M) {
      __afl_prev_loc is thread-local. */
 
   GlobalVariable *AFLMapPtr =
-    new GlobalVariable(M, PointerType::get(Int8Ty, 0), false,
-        GlobalValue::ExternalLinkage, 0, "__afl_area_ptr");
+      new GlobalVariable(M, PointerType::get(Int8Ty, 0), false,
+                         GlobalValue::ExternalLinkage, 0, "__afl_area_ptr");
+
+  GlobalVariable *AFLUCPtr =
+      new GlobalVariable(M, PointerType::get(Int8Ty, 0), false,
+                         GlobalValue::ExternalLinkage, 0, "__afl_uc_ptr");
 
   GlobalVariable *AFLPrevLoc = new GlobalVariable(
       M, Int32Ty, false, GlobalValue::ExternalLinkage, 0, "__afl_prev_loc",
       0, GlobalVariable::GeneralDynamicTLSModel, 0, false);
-
-  /* Open file to read id */
-
-  u32 CurId = 0;
-  s32 Fd  = open("/tmp/.cur_id", O_RDWR | O_CREAT, 0600);;
-  if (Fd < 0) PFATAL("Unable to create /tmp/.cur_id");
-  if (read(Fd, &CurId, 4) < 4) PFATAL("Short read /tmp/.cur_id");
 
   /* Instrument all the things! */
 
@@ -144,7 +138,7 @@ bool AFLCoverage::runOnModule(Module &M) {
 
       LoadInst *PrevLoc = IRB.CreateLoad(AFLPrevLoc);
       PrevLoc->setMetadata(M.getMDKindID("nosanitize"), MDNode::get(C, None));
-      Value *PrevLocCasted = IRB.CreateZExt(PrevLoc, Int32Ty);
+      Value *PrevLocCasted = IRB.CreateZExt(PrevLoc, IRB.getInt32Ty());
 
       /* Load SHM pointer */
 
@@ -169,77 +163,51 @@ bool AFLCoverage::runOnModule(Module &M) {
 
       inst_blocks++;
 
-      /* TODO */
-      CurId += 1;
+    }
 
-      if (BB.size() > 2) {
-        auto It = BB.end();
-        u32 IsConditionalBr = 0;
-        --It;
-        if (BranchInst* BR = dyn_cast<BranchInst>(&(*It))) {
-          IsConditionalBr = BR->isConditional();
-        }
-        if (SwitchInst* SI = dyn_cast<SwitchInst>(&(*It))) {
-          IRBuilder<> IRB(&(*SI));
-          FunctionCallee Insert = M.getOrInsertFunction("insert_distance", VoidTy, Int32Ty, Int32Ty, Int32Ty);
-          Value *Cond = SI->getCondition();
-          if (Cond->getType()->getScalarSizeInBits() <= Int32Ty->getScalarSizeInBits()) {
-            /* Convert label to Index */
-            u32 NumCases = SI->getNumCases() + 1;
-            u32 Labels[NumCases];
-            u32 CurLabel = 1;
-            memset(Labels , 0, sizeof(u32) * NumCases);
-            for (u32 I = 0; I < NumCases; I += 1) {
-              u32 Updated = false;
-              for (u32 J = 0; J < NumCases; J += 1) {
-                Value* A0 = SI->getOperand(1 + 2 * I);
-                Value* A1 = SI->getOperand(1 + 2 * J);
-                if (A0 == A1 && !Labels[J]) {
-                  Labels[J] = CurLabel;
-                  Updated = true;
-                }
-              }
-              if (Updated) CurLabel += 1;
+  std::set<u32> Visited;
+
+  for (auto &F: M)
+    for (auto &BB: F) {
+      auto T = BB.getTerminator();
+      if (T->getNumSuccessors() >= 2) {
+        u32 CurId = 0, NextId = 0;
+        BasicBlock::iterator IP = BB.getFirstInsertionPt();
+        IRBuilder<> IRB(&(*IP));
+        for (auto &Inst : BB) {
+          if (BinaryOperator* OP = dyn_cast<BinaryOperator>(&Inst)) {
+            if (strcmp(OP->getOpcodeName(), "xor") == 0) {
+              Value* A1 = OP->getOperand(1);
+              CurId = dyn_cast<ConstantInt>(A1)->getZExtValue();
+              break;
             }
-            /* Save distance */
-            Value* DefaultDis = ConstantInt::get(Int32Ty, 0);
-            u32 Inc = 0;
-            for (auto It : SI->cases()) {
-              Constant *CaseVal = It.getCaseValue();
-              Value* Distance = IRB.CreateXor(Cond, CaseVal);
-              Distance = IRB.CreateIntCast(Distance, Int32Ty, false);
-              Value* Case = ConstantInt::get(Int32Ty, Labels[Inc + 1]);
-              IRB.CreateCall(Insert, { ConstantInt::get(Int32Ty, CurId), Case, Distance });
-              Value* NotCovered = IRB.CreateICmpNE(Distance, ConstantInt::get(Int32Ty, 0));
-              DefaultDis = IRB.CreateAdd(DefaultDis, NotCovered);
-              Inc += 1;
-            }
-            DefaultDis = IRB.CreateSub(ConstantInt::get(Int32Ty, SI->getNumCases()), DefaultDis);
-            Value* Case = ConstantInt::get(Int32Ty, Labels[0]);
-            IRB.CreateCall(Insert, { ConstantInt::get(Int32Ty, CurId), Case, DefaultDis });
           }
         }
-        --It;
-        if (ICmpInst *ICMP = dyn_cast<ICmpInst>(&(*It))) {
-          if (IsConditionalBr) {
-            IRBuilder<> IRB(&(*ICMP));
-            Value* A0 = ICMP->getOperand(0);
-            Value* A1 = ICMP->getOperand(1);
-            if (A0->getType()->isIntegerTy()) {
-              if (A0->getType()->getScalarSizeInBits() <= Int32Ty->getScalarSizeInBits()) {
-                Value* Distance = IRB.CreateXor(A0, A1);
-                FunctionCallee Insert = M.getOrInsertFunction("insert_distance", VoidTy, Int32Ty, Int32Ty, Int32Ty);
-                Value* Zero = ConstantInt::get(Int32Ty, 0);
-                IRB.CreateCall(Insert, { ConstantInt::get(Int32Ty, CurId), Zero, Distance });
+
+        if (Visited.count(CurId) > 0) break;
+        Visited.insert(CurId);
+
+        for (auto Idx = 0; Idx < T->getNumSuccessors(); Idx += 1) {
+          auto Suc = T->getSuccessor(Idx);
+          for (auto &Inst : *Suc) {
+            if (BinaryOperator* OP = dyn_cast<BinaryOperator>(&Inst)) {
+              if (strcmp(OP->getOpcodeName(), "xor") == 0) {
+                Value* A1 = OP->getOperand(1);
+                NextId = dyn_cast<ConstantInt>(A1)->getZExtValue();
+                break;
               }
             }
+          }
+          if (CurId && NextId) {
+            ConstantInt *CurLoc = ConstantInt::get(Int32Ty, (CurId >> 1) ^ NextId);
+            LoadInst *UCPtr = IRB.CreateLoad(AFLUCPtr);
+            UCPtr->setMetadata(M.getMDKindID("nosanitize"), MDNode::get(C, None));
+            Value *UCPtrIdx = IRB.CreateGEP(UCPtr, CurLoc);
+            IRB.CreateStore(ConstantInt::get(Int8Ty, 1), UCPtrIdx)
+              ->setMetadata(M.getMDKindID("nosanitize"), MDNode::get(C, None));
           }
         }
       }
-
-      FunctionCallee Insert = M.getOrInsertFunction("insert_block", VoidTy, Int32Ty);
-      IRB.CreateCall(Insert, { ConstantInt::get(Int32Ty, CurId) });
-      // errs() << BB;
     }
 
   /* Say something nice. */
@@ -253,10 +221,6 @@ bool AFLCoverage::runOnModule(Module &M) {
               "ASAN/MSAN" : "non-hardened"), inst_ratio);
 
   }
-
-  lseek(Fd, 0, SEEK_SET);
-  if (write(Fd, &CurId, 4) < 4) PFATAL("Short write .cur_id");
-  close(Fd);
 
   return true;
 
