a4eca05b3d29654b916b01a05ff12fc55cf0f9fc
Tai D. Nguyen <duytai.cse@gmail.com>
2021-03-11 11:49:06 +0000

    update compare function

--- llvm_mode/afl-llvm-rt.o.c
+++ llvm_mode/afl-llvm-rt.o.c
@@ -322,16 +322,17 @@ void __sanitizer_cov_trace_pc_guard_init(uint32_t* start, uint32_t* stop) {
 /* Here we capture distance between string
  * Max string len = 256
  * */
-u32 __afl_strncmp(char* x, char* y, u32 max_len) {
+u32 __afl_strncmp(char* x, char* y, u32 len) {
 
   char a[256] = {0}, b[256] = {0};
   u32 total = 0;
-
   u32 x_len = strlen(x);
   u32 y_len = strlen(y);
+
+  if (x_len > 256 || y_len > 256) return -1;
   memcpy(a, x, x_len);
   memcpy(b, y, y_len);
-  for (u32 i = 0; i < 256 & i < max_len; i += 1)
+  for (u32 i = 0; i < 256 && i < len; i += 1)
     total += (a[i] ^ b[i]);
 
   return total;
@@ -343,11 +344,11 @@ u32 __afl_strcmp(char* x, char* y) {
   u32 total = 0;
   u32 x_len = strlen(x);
   u32 y_len = strlen(y);
-  u32 max_len = x_len > y_len ? x_len : y_len;
 
+  if (x_len > 256 || y_len > 256) return -1;
   memcpy(a, x, x_len);
   memcpy(b, y, y_len);
-  for (u32 i = 0; i < 256 & i < max_len; i += 1)
+  for (u32 i = 0; i < 256 && (i < x_len || i < y_len); i += 1)
     total += (a[i] ^ b[i]);
 
   return total;
